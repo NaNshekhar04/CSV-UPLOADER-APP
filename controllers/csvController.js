78e5dbe23e7eec6260d09ac76bdc6f01040a6edc
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-18 19:25:59 +0530

    Converting Actions to async await

--- controllers/csvController.js
+++ controllers/csvController.js
@@ -1,7 +1,7 @@
 const CSVFile = require('../models/csv');
 const fs = require('fs');
 
-module.exports.uploader = function(req, res){
+module.exports.uploader =  function(req, res){
     return res.render('uploadCSV', {
         title: 'CSV | upload',
     });
@@ -9,8 +9,8 @@ module.exports.uploader = function(req, res){
 
 
 //Viewing the File Action
-module.exports.viewFiles = function(req, res){
-    let files =  CSVFile.find({});
+module.exports.viewFiles = async function(req, res){
+    let files =  await CSVFile.find({});
   return res.render('displayCSV', {
     title: 'View Uploaded Files',
     files: files,
@@ -19,11 +19,11 @@ module.exports.viewFiles = function(req, res){
 
 
 //Uploading the File Action
-module.exports.upload = function(req, res){
+module.exports.upload = async function(req, res){
     try{
-    CSVFile.uploadedFile(req, res, (err) => {
+    CSVFile.uploadedFile(req, res, async (err) => {
         if (req.file) {
-          let file =  CSVFile.create(req.file);
+          let file = await CSVFile.create(req.file);
           return res.status(200).json({
             file: file,
           });
@@ -41,9 +41,9 @@ module.exports.upload = function(req, res){
 }
 
 //Deleting the Uploaded File Action
-module.exports.deleteFile = function(req, res){
+module.exports.deleteFile = async function(req, res){
     try{
-    let file = CSVFile.findById(req.params.id);
+    let file = await CSVFile.findById(req.params.id);
     if(file){
         fs.unlinkSync(file.path);
         file.remove();
